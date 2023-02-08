7bdcf5fb15880147a92c4b23884d98b423337635
Marlon Leon <ghostveene@gmail.com>
2023-02-08 14:30:19 -0500

    added ability for input values to be placed on dom

--- index.js
+++ index.js
@@ -35,5 +35,13 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
             }
         })
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(post => {
+
+            blogList.innerHTML = `
+                                    <h3>${post.title}</h3>
+                                    <p>${post.body}</p>
+                                    <hr />
+                                    ${blogList.innerHTML}
+                                    `
+            })
     })
