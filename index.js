8795aa4177248db52d7f993bad840d5a740a65c2
Marlon Leon <ghostveene@gmail.com>
2023-02-08 14:02:05 -0500

    submit button is logging values from input fields

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const blogList = document.getElementById("blog-list")
+const form = document.getElementById("new-post")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+})
+
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
